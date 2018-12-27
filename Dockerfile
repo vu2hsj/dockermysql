423c9a92915527f73422056512dd612ba2959bfe
= <=>
2018-12-27 20:50:18 +0530

    Initial Commit

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM mysql
+
+#ENV MY_SQL_DATABASE test5
+
+
+#COPY ./sql-scripts/ /docker-entrypoint-initdb.d/
+
---
+++ sql-scripts/CreateTable.sql
@@ -0,0 +1,11 @@
+create database test5;
+use test5;
+CREATE TABLE employees4 (
+first_name varchar(25),
+last_name  varchar(25),
+department varchar(15),
+email  varchar(50)
+);
+INSERT INTO employees4(first_name, last_name, department, email) VALUES ('Lorenz', 'Vanthillo', 'IT', 'lvthillo@mail.com');
+INSERT INTO employees4 (first_name, last_name, department, email) VALUES ('Lorenz2', 'Vanthillo 2', 'HR', 'lvthillo@mail.com');
+
